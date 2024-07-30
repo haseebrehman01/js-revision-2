6133b0b2550d225f4fb58a5dd9e1f0d6225a812b
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2024-07-30 17:32:44 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# js-revision-2
+ 
---
+++ app.js
@@ -0,0 +1,7 @@
+// arr.forEach loop through each array 
+let arr = [1,2,3,4,5,6,9] //last wala parameter (orgArray) bhot kaam use hota 
+arr.forEach(function(val,index,orgArray){
+console.log(val,index,orgArray)
+})
+
+
