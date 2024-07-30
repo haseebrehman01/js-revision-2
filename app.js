28c2740261724582efe26ef9e3b4ccb1b1e04f8e
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2024-07-30 18:01:58 +0500

    for each . filter, sort rev complete

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+let arr = [1,40,5,10,5,90,9] 
 // arr.forEach loop through each array 
-let arr = [1,2,3,4,5,6,9] //last wala parameter (orgArray) bhot kaam use hota 
-arr.forEach(function(val,index,orgArray){
-console.log(val,index,orgArray)
-})
+//last wala parameter (orgArray) bhot kaam use hota 
+// arr.forEach(function(val,index,orgArray){
+// console.log(val,index,orgArray)
+// })
 
 
+// arr.filter() // condition mai use hota, true false dekhta, orgarry pa loi farq nahi dalta, naya array deta
+// let result = arr.filter(function(val){
+//     return  val % 2 === 0
+//     //or
+// // if (val % 2 === 0){
+// // return true 
+// // }
+
+// // else{
+// //    return false
+// // }
+// })
+// console.log(result)
+
+
+// //arr.sort() //(gives new arr take 2 params (a,b) )
+// let sortedArr =  arr.sort(function(a,b){
+// if(a>b){ // a<b for big to small
+// return 1
+// }
+// else{
+//     return - 1
+// }
+// })
+// console.log(sortedArr)
+
+
+//arr.reduce
